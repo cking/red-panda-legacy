4219c59c4d6ec6b1d90e37de0d4664f8ca586621
Christopher König <criss@paars.de>
2015-10-30 11:23:31 +0100

    definition :)

--- modules/gimmicks.js
+++ modules/gimmicks.js
@@ -7,10 +7,28 @@ class Gimmicks {
   constructor(deps) {
     let c = this.$commander = deps.commander
     this.$admin = deps.admin
+    this.$db = deps.db
+    this.$bot = deps.bot
     utils.privatify(this)
 
-    for (var k of ['avatar', 'cookie', 'say']) {
+    this.$db.define = this.$db.define || { channels: [], terms: {} }
+    this.$bot.Dispatcher.on('MESSAGE_CREATE', this.messageCreate.bind(this))
+
+    for (var k of ['avatar', 'cookie', 'say', 'define', 'define_cleverclock']) {
       c.registerCommand(k, this[k].bind(this))
+    }
+  }
+
+  messageCreate (data) {
+    let msg = data.message
+    if (msg.author.id === this.$bot.User.id || this.$db.define.channels.indexOf(msg.channel_id) < 0) {
+      return
+    }
+
+    for (let k in this.$db.define.terms) {
+      if (msg.content.match(new RegExp(`(^|\\s)${k}(\s|$)`, 'i'))) {
+        this.define(msg.author, k, msg.channel)
+      }
     }
   }
 
@@ -44,9 +62,55 @@ class Gimmicks {
       reply(args.join(' '))
     }
   }
+
+  define (from, args, reply) {
+    let term = this.$db.define.terms[args[0]]
+    if (args.length === 1) {
+      if (term) {
+        reply(`*Sorry to disturb you, but in my opinion* **${args[0]}** ${term}`)
+      }
+      else {
+        reply('*I tried, I really tried :crying_cat_face: I can\'t find out what ' + args[0] + ' means...*')
+      }
+    }
+
+    if (!this.$admin.can(from.id, 'define')) {
+      reply(`I am sorry ${from.username}, I am afraid i can't let you do that.`)
+    }
+
+    let keyword = args.shift()
+    let definition = args.join(' ')
+
+    this.$db.define.terms[keyword] = definition
+    reply(`Thank you for teaching me senpai! From now on **${keyword}** ${definition}`)
+  }
+
+  define_cleverclock (from, args, reply) {
+    if (!this.$admin.can(from.id, 'define cleverclock')) {
+      reply(`I am sorry ${from.username}, I am afraid i can't let you do that.`)
+    }
+
+    let channel = from.channel
+    if (args.length) {
+      channel = this.$commander.queryOne(args.shift())
+      if (channel.channels) {
+        channel = this.$commander.queryOne(args.shift(), channel)
+      }
+    }
+
+    let idx = this.$db.define.channels.indexOf(channel.id)
+    if (idx < 0) {
+      reply(`From now on, I shall correct all people in #${channel.name} on ${channel.guild.name} *evil laugh*`)
+       this.$db.define.channels.push(channel.id)
+    }
+    else {
+      reply(`Oh, did I annoy you too much? I will stop then...`)
+      this.$db.define.channels.splice(idx, 1)
+    }
+  }
 }
 
 Gimmicks.type = 'class'
-Gimmicks.depends = { commander: 1, admin: 1 }
+Gimmicks.depends = { commander: 1, admin: 1, db: 1, bot: 1 }
 
 module.exports = Gimmicks
